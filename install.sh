c7359a145746c0eab8b2dd9979b050b0cc407591
Brahim Hadriche <brahim.hadriche@gmail.com>
2022-11-06 11:58:29 -0500

    Read input if vars are missing

--- install.sh
+++ install.sh
@@ -3,21 +3,20 @@
 if [ -z "$ROKU_DEV_TARGET" ];
 then
     echo "Variable ROKU_DEV_TARGET is not set. What is your Roku TV IP address? Example: 192.168.1.2";
-    exit -1
+    read ROKU_DEV_TARGET
 fi
 
 if [ -z "$DEVPASSWORD" ];
 then
     echo "Variable DEVPASSWORD is not set. What is your Roku Dev password? Example: 1234";
-    exit -1
+    read DEVPASSWORD
 fi
-
 
 tmp_folder=$(mktemp -d)
 tmp_zip_file="$tmp_folder/roku-youtube.zip"
 tmp_http_response_file="$tmp_folder/roku-response.html"
 
-echo "Downloading archive to temprary folder ..."
+echo "Downloading archive to temporary folder ..."
 
 # TODO: Check for curl, use wget if curl is not available
 curl -L https://github.com/iBicha/roku-youtube/releases/latest/download/roku-youtube.zip -o $tmp_zip_file
