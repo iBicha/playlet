9031f51d5d1ade1e2eaf4c3b88c8687b00ef5b80
Brahim Hadriche <brahim.hadriche@gmail.com>
2022-12-07 12:43:33 -0500

    Add Svelte app boilerplate (#27)
    * Add Svelte app boilerplate
    
    * Delete www folder
    
    * Web app setup
    
    * Fix vector images
    
    * README

---
+++ playlet-web/src/lib/CurrentInstance.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { GetHost } from "./Host";
+
+    let currentInstance = "N/A";
+
+    onMount(async () => {
+        const response = await fetch(`http://${GetHost()}/api/state`);
+        const state = await response.json();
+        currentInstance = state.invidious.selected_instance;
+    })
+
+</script>
+<div>
+    Current Instance: {currentInstance}
+</div>
---
+++ playlet-web/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { readFileSync } from 'fs'
+import { parse as dotEnvParse } from 'dotenv'
+import { internalIpV4Sync } from 'internal-ip';
+
+const envVars = dotEnvParse(readFileSync('../.vscode/.env'))
+
+const PORT = 5173
+
+// https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: "../src/www",
+    emptyOutDir: true
+  },
+  server: {
+    host: true,
+    port: PORT,
+    open: `http://${internalIpV4Sync()}:${PORT}/?host=${envVars.ROKU_DEV_TARGET}:8888`
+  }
+})
