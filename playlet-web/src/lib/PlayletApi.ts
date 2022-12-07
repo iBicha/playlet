87c4e01c19284a03f4f6d22304ff642b530f9881
Brahim Hadriche <brahim.hadriche@gmail.com>
2022-12-07 18:17:26 -0500

    Add apis

--- playlet-web/src/lib/PlayletApi.ts
+++ playlet-web/src/lib/PlayletApi.ts
@@ -4,7 +4,33 @@ export class PlayletApi {
     static host = () => `http://${getHost()}`
 
     static async getState() {
-        const response = await fetch(`${PlayletApi.host()}/api/state`)
-        return await response.json()
+        const response = await fetch(`${PlayletApi.host()}/api/state`);
+        return await response.json();
+    }
+
+    static async logout() {
+        return await PlayletApi.postJson(`${PlayletApi.host()}/api/command`, { command: "logout" });
+    }
+
+    static async playVideo(videoId) {
+        return await PlayletApi.postJson(`${PlayletApi.host()}/api/command`, { command: "play", v: videoId });
+    }
+
+    static async clearSearchHistory() {
+        return await PlayletApi.postJson(`${PlayletApi.host()}/api/command`, { command: "clear-search-history" });
+    }
+
+    static async updateInstances(instances) {
+        return await PlayletApi.postJson(`${PlayletApi.host()}/invidious/instances`, instances);
+    }
+
+    private static postJson(url, payload) {
+        return fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(payload)
+        })
     }
 }
