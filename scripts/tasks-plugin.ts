8d311c5d751c3c13c19b044d723ffb752e2d0b29
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-01-01 15:44:06 -0500

    tweak

--- scripts/tasks-plugin.ts
+++ scripts/tasks-plugin.ts
@@ -1,37 +1,33 @@
 import { CompilerPlugin, BeforeFileTranspileEvent, isBrsFile, WalkMode, createVisitor, TokenKind, VariableExpression, Identifier } from 'brighterscript';
 
-// plugin factory
-export default function () {
-    return {
-        name: 'replacePlaceholders',
-        // transform AST before transpilation
-        beforeFileTranspile: (event: BeforeFileTranspileEvent) => {
-            if (isBrsFile(event.file)) {
-                event.file.ast.walk(createVisitor({
-                    LiteralExpression: (literal) => {
-                        //replace every occurance of <FIRST_NAME> in strings with "world"
-                        if (literal.token.kind === TokenKind.StringLiteral && literal.token.text.includes('<FIRST_NAME>')) {
-                            event.editor.setProperty(literal.token, 'text', literal.token.text.replace('<FIRST_NAME>', 'world'));
-                        }
-                    },
-                    CallExpression: (call) => {
-                        /* @ts-ignore */
-                        const funcName = call.callee.name?.text
-                        if (funcName === "StartTask") {
-                            /* @ts-ignore */
-                            let identifier: Identifier = {
-                                kind: TokenKind.Identifier,
-                                text: "Hi",
-                            }
-                            /* @ts-ignore */
-                            event.editor.arrayPush(call.args, [new VariableExpression(identifier)])
-                            console.log(call)
-                        }
-                    },
-                }), {
-                    walkMode: WalkMode.visitExpressionsRecursive
-                });
-            }
+export class AsyncTaskPlugin implements CompilerPlugin
+{
+    public name = 'asyncTaskPlugin';
+
+    beforeFileTranspile(event: BeforeFileTranspileEvent) {
+        if (!isBrsFile(event.file)) {
+            return
         }
-    } as CompilerPlugin;
+        event.file.ast.walk(createVisitor({
+            LiteralExpression: (literal) => {
+                //replace every occurance of <FIRST_NAME> in strings with "world"
+                if (literal.token.kind === TokenKind.StringLiteral && literal.token.text.includes('<FIRST_NAME>')) {
+                    event.editor.setProperty(literal.token, 'text', literal.token.text.replace('<FIRST_NAME>', 'world'));
+                }
+            },
+            CallExpression: (call) => {
+                /* @ts-ignore */
+                const funcName = call.callee.name?.text
+                if (funcName === "StartTask") {
+                    console.log(funcName)
+                }
+            },
+        }), {
+            walkMode: WalkMode.visitExpressionsRecursive
+        });
+    }
+}
+
+export default () => {
+    return new AsyncTaskPlugin();
 };
