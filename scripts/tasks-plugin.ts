cdf5c07be0985181f5ae361be7b12c14a68f91ed
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-01-01 17:59:09 -0500

    Kinda works

--- scripts/tasks-plugin.ts
+++ scripts/tasks-plugin.ts
@@ -1,7 +1,15 @@
-import { CompilerPlugin, BeforeFileTranspileEvent, isBrsFile, WalkMode, createVisitor, TokenKind, VariableExpression, Identifier } from 'brighterscript';
+import {
+    CompilerPlugin,
+    BscFile,
+    BeforeFileTranspileEvent,
+    isBrsFile,
+    WalkMode,
+    createVisitor,
+    TokenKind,
+    DiagnosticSeverity
+} from 'brighterscript';
 
-export class AsyncTaskPlugin implements CompilerPlugin
-{
+export class AsyncTaskPlugin implements CompilerPlugin {
     public name = 'asyncTaskPlugin';
 
     beforeFileTranspile(event: BeforeFileTranspileEvent) {
@@ -9,22 +17,68 @@ export class AsyncTaskPlugin implements CompilerPlugin
             return
         }
         event.file.ast.walk(createVisitor({
-            LiteralExpression: (literal) => {
-                //replace every occurance of <FIRST_NAME> in strings with "world"
-                if (literal.token.kind === TokenKind.StringLiteral && literal.token.text.includes('<FIRST_NAME>')) {
-                    event.editor.setProperty(literal.token, 'text', literal.token.text.replace('<FIRST_NAME>', 'world'));
+            FunctionExpression: (func) => {
+                const annotations = func.functionStatement?.annotations
+                if (!annotations || annotations.length === 0) {
+                    return
                 }
-            },
-            CallExpression: (call) => {
-                /* @ts-ignore */
-                const funcName = call.callee.name?.text
-                if (funcName === "StartTask") {
-                    console.log(funcName)
+                if (annotations[0].name == "asynctask") {
+                    const functionName = func.functionStatement!.name.text
+                    const taskName = `${functionName}_asynctask`
+                 
+                    const bs = this.generateBsTask(functionName, event.file)
+                    const bsFile = `components/asynctasks/generated/${taskName}.bs`
+                 
+                    const xml = this.generateXmlTask(functionName, taskName, bsFile)
+                    const xmlFile = `components/asynctasks/generated/${taskName}.xml`
+                   
+                    if(event.program.hasFile(xmlFile) || event.program.hasFile(bsFile)){
+                        event.program.addDiagnostics([{
+                            message: `Duplicate async task ${functionName}, use a different name`,
+                            range: annotations[0].nameToken.range,
+                            file: event.file,
+                            severity: DiagnosticSeverity.Error,
+                        }])
+                    }
+
+                    event.program.setFile(xmlFile, xml)
+                    event.program.setFile(bsFile, bs)
                 }
             },
         }), {
             walkMode: WalkMode.visitExpressionsRecursive
         });
+    }
+
+    generateBsTask(functionName: string, file: BscFile): string {
+        return `
+import "pkg:/${file.pkgPath}"
+
+function Init()
+    m.top.functionName = "TaskMain"
+end function
+
+function TaskMain()
+    result = ${functionName}()
+    m.top.setField("output", {
+        result: result
+    })
+end function
+`
+
+    }
+
+    generateXmlTask(functionName: string, taskName: string, bsFile: string): string {
+
+        return `<?xml version="1.0" encoding="UTF-8" ?>
+
+<component name="${taskName}" extends="Task">
+  <interface>
+    <field id="input" type="assocarray" />
+    <field id="output" type="assocarray" />
+  </interface>
+  <script type="text/brightscript" uri="pkg:/${bsFile}" />
+</component>`
     }
 }
 
