f4696fd84ee487668f7e7f41d057d970f4477264
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-01-25 22:01:14 -0500

    Update script

--- scripts/update-manifest-version.js
+++ scripts/update-manifest-version.js
@@ -10,16 +10,18 @@ const packageJson = JSON.parse(packageJsonContent);
 const version = packageJson.version;
 const parsedVersion = semverParse(version);
 
-let appManifestContent = fs.readFileSync("./src/manifest", { encoding: 'utf8', flag: 'r' });
+["playlet/src/manifest", "playlet-lib/src/manifest"].forEach(function (manifestPath) {
+    let appManifestContent = fs.readFileSync(manifestPath, { encoding: 'utf8', flag: 'r' });
 
-const majorPattern = /major_version=(\d+)/;
-const minorPattern = /minor_version=(\d+)/;
-const buildPattern = /build_version=(\d+)/;
+    const majorPattern = /major_version=(\d+)/;
+    const minorPattern = /minor_version=(\d+)/;
+    const buildPattern = /build_version=(\d+)/;
 
-appManifestContent = appManifestContent.replace(majorPattern, `major_version=${parsedVersion.major}`);
-appManifestContent = appManifestContent.replace(minorPattern, `minor_version=${parsedVersion.minor}`);
-appManifestContent = appManifestContent.replace(buildPattern, `build_version=${String(parsedVersion.patch).padStart(5, '0')}`);
+    appManifestContent = appManifestContent.replace(majorPattern, `major_version=${parsedVersion.major}`);
+    appManifestContent = appManifestContent.replace(minorPattern, `minor_version=${parsedVersion.minor}`);
+    appManifestContent = appManifestContent.replace(buildPattern, `build_version=${String(parsedVersion.patch).padStart(5, '0')}`);
 
-fs.writeFileSync("./src/manifest", appManifestContent);
+    fs.writeFileSync(manifestPath, appManifestContent);
+})
 
 execSync(`npm version ${version} --allow-same-version`, { cwd: 'playlet-web' });
