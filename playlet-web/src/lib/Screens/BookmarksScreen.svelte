072b93728a4ed62b999a1cd0e891b46b120783eb
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-10-24 13:41:13 -0400

    Bookmarks web app part 2 (#178)
    * Message for no bookmarks
    
    * feed lazy loading
    
    * cached fetch
    
    * Versioned cache
    
    * comment

--- playlet-web/src/lib/Screens/BookmarksScreen.svelte
+++ playlet-web/src/lib/Screens/BookmarksScreen.svelte
@@ -6,7 +6,21 @@
 </script>
 
 <div class={visibility ? "" : "hidden"}>
-  {#each $bookmarksStore as feed}
-    <VideoListRow {feed} />
-  {/each}
+  {#if $bookmarksStore.length === 0}
+    <div
+      class="flex flex-col items-center justify-start h-screen w-2/3 mx-auto"
+    >
+      <div class="text-2xl font-bold text-gray-500">No Bookmarks</div>
+      <div class="text-gray-500 text-center">
+        You currently have no bookmarks.<br />
+        To add bookmarks, select a video, playlist or channel, and add a bookmark.<br
+        />
+        Please note that Bookmarks is an experimental feature.
+      </div>
+    </div>
+  {:else}
+    {#each $bookmarksStore as feed}
+      <VideoListRow {feed} />
+    {/each}
+  {/if}
 </div>
