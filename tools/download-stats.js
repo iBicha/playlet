1f1a4479da27383421be251e1d55c7b86bcb0ea0
Brahim Hadriche <brahim.hadriche@gmail.com>
2023-11-05 18:53:37 -0500

    stats test (#189)

---
+++ tools/download-stats.js
@@ -0,0 +1,20 @@
+const markdownFile = "../playlet.wiki/Home.md"
+
+const markdownContent = `
+# Playlet stats
+This page was automatically generated on ${new Date().toUTCString()}.
+
+## Channel Health
+
+![Channel Health](Channel_Health.png)
+
+## Channel Engagement
+
+![Channel Engagement](Channel_Engagement.png)
+
+## Viewership Summary
+
+![Viewership Summary](Viewership_Summary.png)
+`
+
+fs.writeFileSync(markdownFile, markdownContent)
