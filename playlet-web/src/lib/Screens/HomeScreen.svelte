fd7bdde7339cb1db8027a53742d1cbb72e822a92
Brahim Hadriche <brahim.hadriche@gmail.com>
2024-01-06 18:17:29 -0500

    Customize home screen (#250)
    * Customize home screen
    
    * remove logs
    
    * Lint fix
    
    * move func
    
    * Fix for checklist
    
    * Fix save state
    
    * open api
    
    * Fix spec
    
    * Changelog
    
    * Add TODO
    
    * Button size
    
    ---------
    
    Co-authored-by: github-action linter <githubaction@githubaction.com>

--- playlet-web/src/lib/Screens/HomeScreen.svelte
+++ playlet-web/src/lib/Screens/HomeScreen.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import VideoListRow from "lib/VideoFeed/VideoListRow.svelte";
-  import { homeLayoutFileStore } from "lib/Stores";
+  import { homeLayoutStore } from "lib/Stores";
 
   export let visibility: boolean;
 </script>
 
 <div class={visibility ? "" : "hidden"}>
   <!-- TODO:P0 Handle authenticated feed when not logged in -->
-  {#each $homeLayoutFileStore as feed}
+  {#each $homeLayoutStore as feed}
     <VideoListRow {feed} />
   {/each}
 </div>
