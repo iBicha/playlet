1d38fef8fe297c5e8ee36515a439e4d531bc10c9
Brahim Hadriche <brahim.hadriche@gmail.com>
2024-03-16 23:08:23 -0400

    Localization test

---
+++ playlet-lib/src/locale/en_US/translations.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Profile</source>
+            <translation>Profile</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Search</translation>
+        </message>
+        <message>
+            <source>Home</source>
+            <translation>Home</translation>
+        </message>
+        <!-- Bookmarks crashes the app -->
+        <!-- <message>
+            <source>Bookmarks</source>
+            <translation>Bookmarks</translation>
+        </message> -->
+        <message>
+            <source>Settings</source>
+            <translation>Settings</translation>
+        </message>
+        <message>
+            <source>Remote</source>
+            <translation>Remote</translation>
+        </message>
+        <message>
+            <source>Info</source>
+            <translation>Info</translation>
+        </message>
+    </context>
+</TS>
---
+++ playlet-lib/src/locale/fr_CA/translations.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Profile</source>
+            <translation>Profil</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Recherche</translation>
+        </message>
+        <message>
+            <source>Home</source>
+            <translation>Accueil</translation>
+        </message>
+        <!-- Bookmarks crashes the app -->
+        <!-- <message>
+            <source>Bookmarks</source>
+            <translation>Favoris</translation>
+        </message> -->
+        <message>
+            <source>Settings</source>
+            <translation>Paramètres</translation>
+        </message>
+        <message>
+            <source>Remote</source>
+            <comment>Télécommande is too long for the UI</comment>
+            <translation>Remote</translation>
+        </message>
+        <message>
+            <source>Info</source>
+            <translation>Info</translation>
+        </message>
+    </context>
+</TS>
