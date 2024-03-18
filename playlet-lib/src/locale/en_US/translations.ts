7d9817994c65f08546381f0aa9346172d82a6ec1
Brahim Hadriche <brahim.hadriche@gmail.com>
2024-03-17 22:22:53 -0400

    Localization test (#331)
    * Localization test
    
    * More translations
    
    * More translations
    
    * Setup the tooling and validation for the localization
    
    * More translations
    
    * Docs

---
+++ playlet-lib/src/locale/en_US/translations.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Profile</source>
+            <translation>Profile</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Search</translation>
+        </message>
+        <message>
+            <source>Home</source>
+            <translation>Home</translation>
+        </message>
+        <message>
+            <source>Bookmarks</source>
+            <translation>Bookmarks</translation>
+        </message>
+        <message>
+            <source>Settings</source>
+            <translation>Settings</translation>
+        </message>
+        <message>
+            <source>Remote</source>
+            <translation>Remote</translation>
+        </message>
+        <message>
+            <source>Info</source>
+            <translation>Info</translation>
+        </message>
+        <message>
+            <source>Activate</source>
+            <translation>Activate</translation>
+        </message>
+        <message>
+            <source>In Use</source>
+            <translation>In Use</translation>
+        </message>
+        <message>
+            <source>Login</source>
+            <translation>Login</translation>
+        </message>
+        <message>
+            <source>Logout</source>
+            <translation>Logout</translation>
+        </message>
+        <message>
+            <source>Close</source>
+            <translation>Close</translation>
+        </message>
+        <message>
+            <source>Filters</source>
+            <translation>Filters</translation>
+        </message>
+        <message>
+            <source>Subscriptions</source>
+            <translation>Subscriptions</translation>
+        </message>
+        <message>
+            <source>Playlists</source>
+            <translation>Playlists</translation>
+        </message>
+        <message>
+            <source>Watch history</source>
+            <translation>Watch history</translation>
+        </message>
+        <message>
+            <source>Trending</source>
+            <translation>Trending</translation>
+        </message>
+        <message>
+            <source>Trending - Movies</source>
+            <translation>Trending - Movies</translation>
+        </message>
+        <message>
+            <source>Trending - Music</source>
+            <translation>Trending - Music</translation>
+        </message>
+        <message>
+            <source>Trending - Gaming</source>
+            <translation>Trending - Gaming</translation>
+        </message>
+        <message>
+            <source>Popular</source>
+            <translation>Popular</translation>
+        </message>
+        <message>
+            <source>Videos</source>
+            <translation>Videos</translation>
+        </message>
+        <message>
+            <source>Shorts</source>
+            <translation>Shorts</translation>
+        </message>
+        <message>
+            <source>Live</source>
+            <translation>Live</translation>
+        </message>
+        <message>
+            <source>Podcasts</source>
+            <translation>Podcasts</translation>
+        </message>
+        <message>
+            <source>Edit</source>
+            <translation>Edit</translation>
+        </message>
+        <message>
+            <source>Save</source>
+            <translation>Save</translation>
+        </message>
+        <message>
+            <source>Test instance</source>
+            <translation>Test instance</translation>
+        </message>
+        <message>
+            <source>Subscribe</source>
+            <translation>Subscribe</translation>
+        </message>
+        <message>
+            <source>Unsubscribe</source>
+            <translation>Unsubscribe</translation>
+        </message>
+        <message>
+            <source>Login to Invidious</source>
+            <translation>Login to Invidious</translation>
+        </message>
+        <message>
+            <source>IP address not available</source>
+            <translation>IP address not available</translation>
+        </message>
+        <message>
+            <source>Scan the QR code</source>
+            <translation>Scan the QR code</translation>
+        </message>
+        <message>
+            <source>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</source>
+            <translation>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</translation>
+        </message>
+        <message>
+            <source>Please note that an Invidious account is different from a YouTube account.</source>
+            <translation>Please note that an Invidious account is different from a YouTube account.</translation>
+        </message>
+        <message>
+            <source>Invidious custom instance</source>
+            <translation>Invidious custom instance</translation>
+        </message>
+        <message>
+            <source>Or</source>
+            <translation>Or</translation>
+        </message>
+        <message>
+            <source>Invidious public instances</source>
+            <translation>Invidious public instances</translation>
+        </message>
+        <message>
+            <source>Move up</source>
+            <translation>Move up</translation>
+        </message>
+        <message>
+            <source>Move down</source>
+            <translation>Move down</translation>
+        </message>
+
+    </context>
+</TS>
---
+++ playlet-lib/src/locale/fr_CA/translations.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Profile</source>
+            <translation>Profil</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Recherche</translation>
+        </message>
+        <message>
+            <source>Home</source>
+            <translation>Accueil</translation>
+        </message>
+        <message>
+            <source>Bookmarks</source>
+            <translation>Favoris</translation>
+        </message>
+        <message>
+            <source>Settings</source>
+            <translation>Paramètres</translation>
+        </message>
+        <message>
+            <source>Remote</source>
+            <comment>Télécommande is too long for the UI</comment>
+            <translation>Commande</translation>
+        </message>
+        <message>
+            <source>Info</source>
+            <translation>Info</translation>
+        </message>
+        <message>
+            <source>Activate</source>
+            <translation>Activer</translation>
+        </message>
+        <message>
+            <source>In Use</source>
+            <translation>Actif</translation>
+        </message>
+        <message>
+            <source>Login</source>
+            <translation>Se connecter</translation>
+        </message>
+        <message>
+            <source>Logout</source>
+            <translation>Se déconnecter</translation>
+        </message>
+        <message>
+            <source>Close</source>
+            <translation>Fermer</translation>
+        </message>
+        <message>
+            <source>Filters</source>
+            <translation>Filtres</translation>
+        </message>
+        <message>
+            <source>Subscriptions</source>
+            <translation>Abonnements</translation>
+        </message>
+        <message>
+            <source>Playlists</source>
+            <translation>Playlists</translation>
+        </message>
+        <message>
+            <source>Watch history</source>
+            <translation>Historique</translation>
+        </message>
+        <message>
+            <source>Trending</source>
+            <translation>Tendances</translation>
+        </message>
+        <message>
+            <source>Trending - Movies</source>
+            <translation>Tendances - Films</translation>
+        </message>
+        <message>
+            <source>Trending - Music</source>
+            <translation>Tendances - Musique</translation>
+        </message>
+        <message>
+            <source>Trending - Gaming</source>
+            <translation>Tendances - Jeux vidéo</translation>
+        </message>
+        <message>
+            <source>Popular</source>
+            <translation>Populaire</translation>
+        </message>
+        <message>
+            <source>Videos</source>
+            <translation>Vidéos</translation>
+        </message>
+        <message>
+            <source>Shorts</source>
+            <translation>Shorts</translation>
+        </message>
+        <message>
+            <source>Live</source>
+            <translation>En direct</translation>
+        </message>
+        <message>
+            <source>Podcasts</source>
+            <translation>Podcasts</translation>
+        </message>
+        <message>
+            <source>Edit</source>
+            <translation>Modifier</translation>
+        </message>
+        <message>
+            <source>Save</source>
+            <translation>Enregistrer</translation>
+        </message>
+        <message>
+            <source>Test instance</source>
+            <translation>Tester l'instance</translation>
+        </message>
+        <message>
+            <source>Subscribe</source>
+            <translation>S'abonner</translation>
+        </message>
+        <message>
+            <source>Unsubscribe</source>
+            <translation>Se désabonner</translation>
+        </message>
+        <message>
+            <source>Login to Invidious</source>
+            <translation>Se connecter à Invidious</translation>
+        </message>
+        <message>
+            <source>IP address not available</source>
+            <translation>Adresse IP non disponible</translation>
+        </message>
+        <message>
+            <source>Scan the QR code</source>
+            <translation>Scannez le code QR</translation>
+        </message>
+        <message>
+            <source>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</source>
+            <translation>Connectez-vous à Invidious pour consulter les abonnements, les playlists et utiliser l'historique de visionnage.</translation>
+        </message>
+        <message>
+            <source>Please note that an Invidious account is different from a YouTube account.</source>
+            <translation>Veuillez noter qu'un compte Invidious est différent d'un compte YouTube.</translation>
+        </message>
+        <message>
+            <source>Invidious custom instance</source>
+            <translation>Instance personnalisée d'Invidious</translation>
+        </message>
+        <message>
+            <source>Or</source>
+            <translation>Ou</translation>
+        </message>
+        <message>
+            <source>Invidious public instances</source>
+            <translation>Instances publiques d'Invidious</translation>
+        </message>
+        <message>
+            <source>Move up</source>
+            <translation>Monter</translation>
+        </message>
+        <message>
+            <source>Move down</source>
+            <translation>Descendre</translation>
+        </message>
+    </context>
+</TS>
