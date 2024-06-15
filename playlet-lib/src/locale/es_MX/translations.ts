afe5659d5389d4c513cf6cf47da2d10073dc0f2b
Jean Carton <intrinsiccyokai@users.noreply.hosted.weblate.org>
2024-06-15 17:53:03 +0000

    Translated using Weblate (Spanish (Mexico))
    Currently translated at 12.3% (39 of 316 strings)
    
    Translation: Playlet/Playlet
    Translate-URL: https://hosted.weblate.org/projects/playlet/playlet/es_MX/

--- playlet-lib/src/locale/es_MX/translations.ts
+++ playlet-lib/src/locale/es_MX/translations.ts
@@ -1,8 +1,164 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="es_MX" sourcelanguage="en_US">
-    <defaultcodec>UTF-8</defaultcodec>
-    <context>
-        <name>default</name>
-    </context>
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>default</name>
+    <message>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Remote</source>
+        <translation>Remoto</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filtros</translation>
+    </message>
+    <message>
+        <source>Trending - Music</source>
+        <translation>Tendencias - Música</translation>
+    </message>
+    <message>
+        <source>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</source>
+        <translation>Accede a Invidious para ver tus suscripciones, playlists, y usar el historial de reproducción.</translation>
+    </message>
+    <message>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Activate</source>
+        <translation>Activar</translation>
+    </message>
+    <message>
+        <source>In Use</source>
+        <translation>En uso</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Ingresar</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Cerrar Sesión</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <source>Subscriptions</source>
+        <translation>Suscripciones</translation>
+    </message>
+    <message>
+        <source>Watch history</source>
+        <translation>Historial de reproducción</translation>
+    </message>
+    <message>
+        <source>Trending</source>
+        <translation>Tendencias</translation>
+    </message>
+    <message>
+        <source>Trending - Movies</source>
+        <translation>Tendencias - Películas</translation>
+    </message>
+    <message>
+        <source>Trending - Gaming</source>
+        <translation>Tendencias - Videojuegos</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Suscribirse</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Desuscribirse</translation>
+    </message>
+    <message>
+        <source>Login to Invidious</source>
+        <translation>Acceder a Invidious</translation>
+    </message>
+    <message>
+        <source>IP address not available</source>
+        <translation>Dirección IP no disponible</translation>
+    </message>
+    <message>
+        <source>Scan the QR code</source>
+        <translation>Escanea el código QR</translation>
+    </message>
+    <message>
+        <source>Do you want to exit Playlet?</source>
+        <translation>Quieres salir de Playlet?</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Más reciente</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>No se encontraron resultados</translation>
+    </message>
+    <message>
+        <source>Move up</source>
+        <translation>Mover arriba</translation>
+    </message>
+    <message>
+        <source>Move down</source>
+        <translation>Mover abajo</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Sí</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <translation>Éxito</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Borrar</translation>
+    </message>
+    <message>
+        <source>Press &apos;OK&apos; for details</source>
+        <translation>Presiona &apos;OK&apos; para detalles</translation>
+    </message>
+</context>
 </TS>
