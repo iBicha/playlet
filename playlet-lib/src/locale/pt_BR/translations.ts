3fd3db5710d13fd5e43e7b65b0b8821a31b6aa93
Weblate (bot) <hosted@weblate.org>
2024-06-23 19:07:25 +0200

    Translations update from Hosted Weblate (#410)
    * Translated using Weblate (Spanish (Mexico))
    
    Currently translated at 75.0% (237 of 316 strings)
    
    Translation: Playlet/Playlet
    Translate-URL: https://hosted.weblate.org/projects/playlet/playlet/es_MX/
    
    * Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 8.8% (28 of 316 strings)
    
    Translation: Playlet/Playlet
    Translate-URL: https://hosted.weblate.org/projects/playlet/playlet/pt_BR/
    
    ---------
    
    Co-authored-by: Jean Carton <intrinsiccyokai@users.noreply.hosted.weblate.org>
    Co-authored-by: Bruno Fernandes <realbruno@tuta.io>

--- playlet-lib/src/locale/pt_BR/translations.ts
+++ playlet-lib/src/locale/pt_BR/translations.ts
@@ -1,8 +1,120 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="pt_BR" sourcelanguage="en_US">
-    <defaultcodec>UTF-8</defaultcodec>
-    <context>
-        <name>default</name>
-    </context>
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>default</name>
+    <message>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Pesquisar</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Início</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Favoritos</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+    <message>
+        <source>Remote</source>
+        <translation>Controle</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Informações</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Playlists</source>
+        <translation>Playlists</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Vídeos</translation>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>Ao Vivo</translation>
+    </message>
+    <message>
+        <source>Releases</source>
+        <translation>Lançamentos</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <source>Test instance</source>
+        <translation>Testar instância</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Inscrever-se</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Desinscrever-se</translation>
+    </message>
+    <message>
+        <source>Login to Invidious</source>
+        <translation>Logar no Invidious</translation>
+    </message>
+    <message>
+        <source>IP address not available</source>
+        <translation>Endereço de IP indisponível</translation>
+    </message>
+    <message>
+        <source>Activate</source>
+        <translation>Ativar</translation>
+    </message>
+    <message>
+        <source>In Use</source>
+        <translation>Em Uso</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filtros</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Sair da conta</translation>
+    </message>
+    <message>
+        <source>Podcasts</source>
+        <translation>Podcasts</translation>
+    </message>
+</context>
 </TS>
