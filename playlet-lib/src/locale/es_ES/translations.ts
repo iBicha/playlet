ad8f78713704bcf437490c6a746ea3ccc2665fbf
Weblate (bot) <hosted@weblate.org>
2024-08-03 05:01:53 +0200

    Translated using Weblate (Spanish) (#432)
    Currently translated at 31.1% (99 of 318 strings)
    
    Translation: Playlet/Playlet
    Translate-URL: https://hosted.weblate.org/projects/playlet/playlet/es/
    
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>

--- playlet-lib/src/locale/es_ES/translations.ts
+++ playlet-lib/src/locale/es_ES/translations.ts
@@ -1,8 +1,404 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="es_ES" sourcelanguage="en_US">
-    <defaultcodec>UTF-8</defaultcodec>
-    <context>
-        <name>default</name>
-    </context>
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>default</name>
+    <message>
+        <source>Profile</source>
+        <translation>Perfil</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Marcadores</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Remote</source>
+        <translation>Remoto</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Información</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>De acuerdo</translation>
+    </message>
+    <message>
+        <source>Activate</source>
+        <translation>Activar</translation>
+    </message>
+    <message>
+        <source>In Use</source>
+        <translation>En uso</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Inicio de sesión</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Cerrar sesión</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Filters</source>
+        <translation>Filtros</translation>
+    </message>
+    <message>
+        <source>Subscriptions</source>
+        <translation>Suscripciones</translation>
+    </message>
+    <message>
+        <source>Playlists</source>
+        <translation>Listas de reproducción</translation>
+    </message>
+    <message>
+        <source>Watch history</source>
+        <translation>Ver el historial</translation>
+    </message>
+    <message>
+        <source>Trending</source>
+        <translation>Tendencias</translation>
+    </message>
+    <message>
+        <source>Trending - Movies</source>
+        <translation>Tendencias - Películas</translation>
+    </message>
+    <message>
+        <source>Trending - Music</source>
+        <translation>Tendencias - Música</translation>
+    </message>
+    <message>
+        <source>Trending - Gaming</source>
+        <translation>Tendencias - Juegos</translation>
+    </message>
+    <message>
+        <source>Popular</source>
+        <translation>Popular</translation>
+    </message>
+    <message>
+        <source>Videos</source>
+        <translation>Vídeos</translation>
+    </message>
+    <message>
+        <source>Shorts</source>
+        <translation>Vídeos cortos</translation>
+    </message>
+    <message>
+        <source>Live</source>
+        <translation>Directos</translation>
+    </message>
+    <message>
+        <source>Releases</source>
+        <translation>Publicaciones</translation>
+    </message>
+    <message>
+        <source>Podcasts</source>
+        <translation>Podcasts</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Test instance</source>
+        <translation>Probar instancia</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Suscribirte</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Cancelar suscripción</translation>
+    </message>
+    <message>
+        <source>Login to Invidious</source>
+        <translation>Conectarse a Invidious</translation>
+    </message>
+    <message>
+        <source>IP address not available</source>
+        <translation>Dirección IP no disponible</translation>
+    </message>
+    <message>
+        <source>Scan the QR code</source>
+        <translation>Escanear el código QR</translation>
+    </message>
+    <message>
+        <source>Please note that an Invidious account is different from a YouTube account.</source>
+        <translation>Por favor, ten en cuenta que una cuenta de Invidious es diferente de una cuenta de YouTube.</translation>
+    </message>
+    <message>
+        <source>Do you want to exit Playlet?</source>
+        <translation>¿Quieres salir de Playlet?</translation>
+    </message>
+    <message>
+        <source>Sort %1 by</source>
+        <translation>Ordenar %1 por</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Más reciente</translation>
+    </message>
+    <message>
+        <source>Oldest</source>
+        <translation>Más antiguo</translation>
+    </message>
+    <message>
+        <source>Date added (newest)</source>
+        <translation>Fecha en que se añadió (más reciente)</translation>
+    </message>
+    <message>
+        <source>Last video added</source>
+        <translation>Último vídeo añadido</translation>
+    </message>
+    <message>
+        <source>Channel load error</source>
+        <translation>Error al cargar el canal</translation>
+    </message>
+    <message>
+        <source>Failed to load channel %1</source>
+        <translation>Error al cargar el canal %1</translation>
+    </message>
+    <message>
+        <source>Playlist load error</source>
+        <translation>Error al carga la lista de reproducción</translation>
+    </message>
+    <message>
+        <source>Failed to load playlist %1</source>
+        <translation>Error al cargar la lista de reproducción %1</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>Sin resultados</translation>
+    </message>
+    <message>
+        <source>We didn&apos;t find any results for your search. Please try again with a different query or different filters.</source>
+        <translation>No hemos encontrado ningún resultado para tu búsqueda. Por favor, inténtalo de nuevo con una consulta diferente o filtros diferentes.</translation>
+    </message>
+    <message>
+        <source>Invidious public instances load error</source>
+        <translation>Error deal carga las instancias públicas de Invidious</translation>
+    </message>
+    <message>
+        <source>Failed to load Invidious public instances</source>
+        <translation>Error al cargar instancias públicas de Invidious</translation>
+    </message>
+    <message>
+        <source>Failed to load feed</source>
+        <translation>Fallo al cargar el feed</translation>
+    </message>
+    <message>
+        <source>User logged out</source>
+        <translation>Usuario desconectado</translation>
+    </message>
+    <message>
+        <source>Lounge service setup error</source>
+        <translation>Error de configuración del servicio Lounge</translation>
+    </message>
+    <message>
+        <source>Failed to get screen ID. Please restart Playlet to use the cast function.</source>
+        <translation>No se pudo obtener el ID de tu pantalla. Por favor reinicia Playlet para usar la función de transmitir.</translation>
+    </message>
+    <message>
+        <source>Not a valid screen id: %1</source>
+        <translation>Id de pantalla no válida: %1</translation>
+    </message>
+    <message>
+        <source>Failed to get lounge token. Please restart Playlet to use the cast function.</source>
+        <translation>No se ha podido obtener el token dela sala. Por favor, reinicia Playlet para utilizar la función de la transmisión.</translation>
+    </message>
+    <message>
+        <source>Failed to get session data. Please restart Playlet to use the cast function.</source>
+        <translation>No se han podido obtener los datos de la sesión. Por favor, reinicia Playlet para utilizar la función de transmisión.</translation>
+    </message>
+    <message>
+        <source>Invalid response: %1</source>
+        <translation>Respuesta no válida: %1</translation>
+    </message>
+    <message>
+        <source>Invidious custom instance</source>
+        <translation>Instancia personalizada de Invidious</translation>
+    </message>
+    <message>
+        <source>Or</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <source>Invidious public instances</source>
+        <translation>Instancias públicas de Invidious</translation>
+    </message>
+    <message>
+        <source>Move up</source>
+        <translation>Subir</translation>
+    </message>
+    <message>
+        <source>Move down</source>
+        <translation>Bajar</translation>
+    </message>
+    <message>
+        <source>name</source>
+        <translation>nombre</translation>
+    </message>
+    <message>
+        <source>version</source>
+        <translation>versión</translation>
+    </message>
+    <message>
+        <source>users</source>
+        <translation>usuarios</translation>
+    </message>
+    <message>
+        <source>signup</source>
+        <translation>inscríbete</translation>
+    </message>
+    <message>
+        <source>location</source>
+        <translation>ubicación</translation>
+    </message>
+    <message>
+        <source>health</source>
+        <translation>salud</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Sí</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <translation>Éxito</translation>
+    </message>
+    <message>
+        <source>Play next video</source>
+        <translation>Reproducir el siguiente vídeo</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Limpiar</translation>
+    </message>
+    <message>
+        <source>Test</source>
+        <translation>Prueba</translation>
+    </message>
+    <message>
+        <source>Testing %1</source>
+        <translation>Probando %1</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Estado</translation>
+    </message>
+    <message>
+        <source>Test duration</source>
+        <translation>Duración de la prueba</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Detalles</translation>
+    </message>
+    <message>
+        <source>Valid URL</source>
+        <translation>URL verificada</translation>
+    </message>
+    <message>
+        <source>Fetch video details</source>
+        <translation>Obtener detalles del vídeo</translation>
+    </message>
+    <message>
+        <source>Instance reachable</source>
+        <translation>Instancia accesible</translation>
+    </message>
+    <message>
+        <source>Fetch video comments</source>
+        <translation>Obtener comentarios del video</translation>
+    </message>
+    <message>
+        <source>Fetch video captions</source>
+        <translation>Obtener subtítulos del video</translation>
+    </message>
+    <message>
+        <source>Fetch video thumbnails</source>
+        <translation>Obtener miniaturas del video</translation>
+    </message>
+    <message>
+        <source>Url must start with http:// or https://</source>
+        <translation>La URL debe comenzar con http:// o https://</translation>
+    </message>
+    <message>
+        <source>Press &apos;OK&apos; for details</source>
+        <translation>Presiona &apos;De acuerdo&apos; para más detalles</translation>
+    </message>
+    <message>
+        <source>The url seems to be of a valid format</source>
+        <translation>La URL parece tener un formato válido</translation>
+    </message>
+    <message>
+        <source>Invalid JSON from %1 (couldn&apos;t read %2)</source>
+        <translation>JSON no válido para %1 (no se pudo leer %2)</translation>
+    </message>
+    <message>
+        <source>It looks like you are logged in to Invidious, but do not have the required permissions.\nFor that reason, we have logged you out from the following profiles: %1\nPlease log in again.\nThis can happen when Playlet is updated with new features.\nWe apologize for the inconvenience.</source>
+        <translation>Parece que has iniciado sesión en Invidious, pero no tienes los permisos necesarios.\nPor este motivo, hemos cerrado tu sesión en los siguientes perfiles: %1\nPor favor, inicia sesión de nuevo.\nEsto puede suceder cuando Playlet se actualiza con nuevas características.\nDisculpa las molestias.</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Recargar</translation>
+    </message>
+    <message>
+        <source>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</source>
+        <translation>Inicia sesión en Invidious para ver las suscripciones, las listas de reproducción y utilizar el historial.</translation>
+    </message>
+    <message>
+        <source>Fetch with CORS</source>
+        <translation>Obtener con CORS</translation>
+    </message>
+    <message>
+        <source>Url must not end with /</source>
+        <translation>La URL no debe terminar con /</translation>
+    </message>
+    <message>
+        <source>Invalid JSON from %1</source>
+        <translation>JSON no válido para %1</translation>
+    </message>
+    <message>
+        <source>The instance is reachable and running %1 version %2</source>
+        <translation>La instancia es accesible y está utilizando %1 versión %2</translation>
+    </message>
+    <message>
+        <source>The instance can fetch video details for ID %1 (%2)</source>
+        <translation>La instancia puede obtener detalles del video para ID %1 (%2)</translation>
+    </message>
+</context>
 </TS>
