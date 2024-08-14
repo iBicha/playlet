3139118bfe06797b124dfd7ac004948d85f572a1
davbrasan <davidbrazps2@gmail.com>
2024-08-14 14:07:44 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 18.8% (60 of 318 strings)
    
    Translation: Playlet/Playlet
    Translate-URL: https://hosted.weblate.org/projects/playlet/playlet/pt_BR/

--- playlet-lib/src/locale/pt_BR/translations.ts
+++ playlet-lib/src/locale/pt_BR/translations.ts
@@ -50,7 +50,7 @@
     </message>
     <message>
         <source>Playlists</source>
-        <translation>Playlists</translation>
+        <translation>Lista de reprodução</translation>
     </message>
     <message>
         <source>Videos</source>
@@ -98,7 +98,7 @@
     </message>
     <message>
         <source>Login</source>
-        <translation>Login</translation>
+        <translation>Entrar</translation>
     </message>
     <message>
         <source>Filters</source>
@@ -115,6 +115,134 @@
     <message>
         <source>Podcasts</source>
         <translation>Podcasts</translation>
+    </message>
+    <message>
+        <source>Last video added</source>
+        <translation>Último vídeo adicionado</translation>
+    </message>
+    <message>
+        <source>Channel load error</source>
+        <translation>Erro ao carregar o canal</translation>
+    </message>
+    <message>
+        <source>Failed to load channel %1</source>
+        <translation>Falha ao carregar o canal %1</translation>
+    </message>
+    <message>
+        <source>Failed to get lounge token. Please restart Playlet to use the cast function.</source>
+        <translation>Não conseguiu obter token lounge. Por favor, reinicie Playlet para usar a função de cast.</translation>
+    </message>
+    <message>
+        <source>Subscriptions</source>
+        <translation>Inscrições</translation>
+    </message>
+    <message>
+        <source>Watch history</source>
+        <translation>Histórico de assistidos</translation>
+    </message>
+    <message>
+        <source>Trending</source>
+        <translation>Em alta</translation>
+    </message>
+    <message>
+        <source>Trending - Movies</source>
+        <translation>Em alta - Flimes</translation>
+    </message>
+    <message>
+        <source>Trending - Music</source>
+        <translation>Em alta - Música</translation>
+    </message>
+    <message>
+        <source>Trending - Gaming</source>
+        <translation>Em alta - Jogos</translation>
+    </message>
+    <message>
+        <source>Popular</source>
+        <translation>Popular</translation>
+    </message>
+    <message>
+        <source>Shorts</source>
+        <translation>Shorts</translation>
+    </message>
+    <message>
+        <source>Scan the QR code</source>
+        <translation>Digitalize o código QR</translation>
+    </message>
+    <message>
+        <source>Login to Invidious in order to view Subscriptions, Playlists, and use the Watch history.</source>
+        <translation>Faça login no Invidious para visualizar Inscrições, Listas de reprodução e usar o histórico de assistidos.</translation>
+    </message>
+    <message>
+        <source>Please note that an Invidious account is different from a YouTube account.</source>
+        <translation>Por favor, note que uma conta Invidious é diferente de uma conta do YouTube.</translation>
+    </message>
+    <message>
+        <source>Do you want to exit Playlet?</source>
+        <translation>Você quer sair do Playlet?</translation>
+    </message>
+    <message>
+        <source>Sort %1 by</source>
+        <translation>Ordenar por %1</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Mais recente</translation>
+    </message>
+    <message>
+        <source>Oldest</source>
+        <translation>Mais antigo</translation>
+    </message>
+    <message>
+        <source>Date added (newest)</source>
+        <translation>Data da adição (mais recente)</translation>
+    </message>
+    <message>
+        <source>Playlist load error</source>
+        <translation>Erro ao carregar a lista de reprodução</translation>
+    </message>
+    <message>
+        <source>Failed to load playlist %1</source>
+        <translation>Erro ao carregar a lista de reprodução %1</translation>
+    </message>
+    <message>
+        <source>No results found</source>
+        <translation>Nenhum resultado encontrado</translation>
+    </message>
+    <message>
+        <source>We didn&apos;t find any results for your search. Please try again with a different query or different filters.</source>
+        <translation>Não encontramos nenhum resultado para sua pesquisa. Por favor, tente novamente com uma consulta diferente ou filtros diferentes.</translation>
+    </message>
+    <message>
+        <source>Invidious public instances load error</source>
+        <translation>Erro ao carregar a instâncias públicas Invidious</translation>
+    </message>
+    <message>
+        <source>Failed to load Invidious public instances</source>
+        <translation>Erro ao carregar a instância pública Invidious</translation>
+    </message>
+    <message>
+        <source>Failed to load feed</source>
+        <translation>Erro ao carregar o feed</translation>
+    </message>
+    <message>
+        <source>User logged out</source>
+        <translation>Usuário desconectado</translation>
+    </message>
+    <message>
+        <source>It looks like you are logged in to Invidious, but do not have the required permissions.\nFor that reason, we have logged you out from the following profiles: %1\nPlease log in again.\nThis can happen when Playlet is updated with new features.\nWe apologize for the inconvenience.</source>
+        <translation>Parece que você está logado no Invidious, mas não tem as permissões necessárias.\nPor esse motivo, nós o desconectamos dos seguintes perfis: %1\nPor favor, faça login novamente.\nIsso pode acontecer quando o Playlet é atualizado com novos recursos.\nPedimos desculpas pelo inconveniente.</translation>
+    </message>
+    <message>
+        <source>Lounge service setup error</source>
+        <translation>Erro ao configurar o serviço de lounge</translation>
+    </message>
+    <message>
+        <source>Failed to get screen ID. Please restart Playlet to use the cast function.</source>
+        <translation>Falha ao obter o ID da tela. Por favor, reinicie Playlet para usar a função de cast.</translation>
+    </message>
+    <message>
+        <source>Not a valid screen id: %1</source>
+        <translation>Não é um id de tela válido: %1</translation>
     </message>
 </context>
 </TS>
