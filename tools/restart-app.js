3ca87dc1d452db3781ec9cf3e2049e7990973d29
Brahim Hadriche <brahim.hadriche@gmail.com>
2025-06-17 13:24:00 -0400

    Fix issue with deep linking not working due to session data race condition

--- tools/restart-app.js
+++ tools/restart-app.js
@@ -11,15 +11,20 @@ const config = getEnvVars(['ROKU_DEV_TARGET']);
     });
 
     parser.add_argument('--app-id', { help: 'App Id' });
+    parser.add_argument('--query', { help: 'Query parameters to append to the URL' });
 
     const args = parser.parse_args();
     let appId = args.app_id || "dev";
+    const query = args.query || "";
 
     if (appId === "prod") {
         appId = "693751";
     }
 
-    const url = `http://${config.ROKU_DEV_TARGET}:8060/launch/${appId}?restart=true`;
+    let url = `http://${config.ROKU_DEV_TARGET}:8060/launch/${appId}?restart=true`;
+    if (query) {
+        url += `&${query}`;
+    }
     console.log("Restarting app", appId);
     await fetch(url, {
         method: 'POST',
